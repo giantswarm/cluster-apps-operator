cabba3021bcf58a8ac263e7658a362d378ef3f9b
Ross Fairbanks <ross@rossfairbanks.com>
2021-05-11 17:29:47 +0200

    Remove resources except for appversionlabel

--- service/controller/cluster.go
+++ service/controller/cluster.go
@@ -2,26 +2,19 @@ package controller
 
 import (
 	"github.com/giantswarm/k8sclient/v5/pkg/k8sclient"
-	"github.com/giantswarm/k8smetadata/pkg/annotation"
 	"github.com/giantswarm/k8smetadata/pkg/label"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/giantswarm/operatorkit/v4/pkg/controller"
 	"github.com/giantswarm/operatorkit/v4/pkg/resource"
-	"github.com/giantswarm/operatorkit/v4/pkg/resource/crud"
-	"github.com/giantswarm/operatorkit/v4/pkg/resource/k8s/configmapresource"
 	"github.com/giantswarm/operatorkit/v4/pkg/resource/wrapper/metricsresource"
 	"github.com/giantswarm/operatorkit/v4/pkg/resource/wrapper/retryresource"
-	"github.com/giantswarm/resource/v2/appresource"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	apiv1alpha3 "sigs.k8s.io/cluster-api/api/v1alpha3"
 
 	"github.com/giantswarm/cluster-apps-operator/pkg/project"
-	"github.com/giantswarm/cluster-apps-operator/service/controller/resource/app"
-	"github.com/giantswarm/cluster-apps-operator/service/controller/resource/appfinalizer"
 	"github.com/giantswarm/cluster-apps-operator/service/controller/resource/appversionlabel"
-	"github.com/giantswarm/cluster-apps-operator/service/controller/resource/clusterconfigmap"
 	"github.com/giantswarm/cluster-apps-operator/service/internal/podcidr"
 	"github.com/giantswarm/cluster-apps-operator/service/internal/releaseversion"
 )
@@ -86,64 +79,6 @@ func NewCluster(config ClusterConfig) (*Cluster, error) {
 func newClusterResources(config ClusterConfig) ([]resource.Interface, error) {
 	var err error
 
-	var appGetter appresource.StateGetter
-	{
-		c := app.Config{
-			G8sClient:      config.K8sClient.G8sClient(),
-			K8sClient:      config.K8sClient.K8sClient(),
-			Logger:         config.Logger,
-			ReleaseVersion: config.ReleaseVersion,
-
-			RawAppDefaultConfig:  config.RawAppDefaultConfig,
-			RawAppOverrideConfig: config.RawAppOverrideConfig,
-		}
-
-		appGetter, err = app.New(c)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-	}
-
-	var appResource resource.Interface
-	{
-		c := appresource.Config{
-			G8sClient: config.K8sClient.G8sClient(),
-			Logger:    config.Logger,
-
-			Name:        app.Name,
-			StateGetter: appGetter,
-		}
-
-		c.AllowedAnnotations = []string{
-			annotation.AppOperatorLatestConfigMapVersion,
-			annotation.AppOperatorLatestSecretVersion,
-		}
-
-		ops, err := appresource.New(c)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-
-		appResource, err = toCRUDResource(config.Logger, ops)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-	}
-
-	var appFinalizerResource resource.Interface
-	{
-		c := appfinalizer.Config{
-			G8sClient: config.K8sClient.G8sClient(),
-			K8sClient: config.K8sClient.K8sClient(),
-			Logger:    config.Logger,
-		}
-
-		appFinalizerResource, err = appfinalizer.New(c)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-	}
-
 	var appVersionLabelResource resource.Interface
 	{
 		c := appversionlabel.Config{
@@ -158,53 +93,8 @@ func newClusterResources(config ClusterConfig) ([]resource.Interface, error) {
 		}
 	}
 
-	var clusterConfigMapGetter configmapresource.StateGetter
-	{
-		c := clusterconfigmap.Config{
-			K8sClient: config.K8sClient.K8sClient(),
-			Logger:    config.Logger,
-			PodCIDR:   config.PodCIDR,
-
-			BaseDomain:     config.BaseDomain,
-			ClusterIPRange: config.ClusterIPRange,
-			DNSIP:          config.DNSIP,
-		}
-
-		clusterConfigMapGetter, err = clusterconfigmap.New(c)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-	}
-
-	var clusterConfigMapResource resource.Interface
-	{
-		c := configmapresource.Config{
-			K8sClient: config.K8sClient.K8sClient(),
-			Logger:    config.Logger,
-
-			AllowedLabels: []string{
-				label.AppOperatorWatching,
-			},
-			Name:        clusterconfigmap.Name,
-			StateGetter: clusterConfigMapGetter,
-		}
-
-		ops, err := configmapresource.New(c)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-
-		clusterConfigMapResource, err = toCRUDResource(config.Logger, ops)
-		if err != nil {
-			return nil, microerror.Mask(err)
-		}
-	}
-
 	resources := []resource.Interface{
-		appFinalizerResource,
-		appResource,
 		appVersionLabelResource,
-		clusterConfigMapResource,
 	}
 
 	{
@@ -228,18 +118,4 @@ func newClusterResources(config ClusterConfig) ([]resource.Interface, error) {
 	}
 
 	return resources, nil
-}
-
-func toCRUDResource(logger micrologger.Logger, v crud.Interface) (*crud.Resource, error) {
-	c := crud.ResourceConfig{
-		CRUD:   v,
-		Logger: logger,
-	}
-
-	r, err := crud.NewResource(c)
-	if err != nil {
-		return nil, microerror.Mask(err)
-	}
-
-	return r, nil
 }
