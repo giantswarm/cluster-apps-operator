0b036342cb8df0428a0570e0a8c65c32ae9df319
Ross Fairbanks <ross@rossfairbanks.com>
2021-05-18 12:34:39 +0200

    Address review comments

--- service/controller/resource/clusterconfigmap/desired.go
+++ service/controller/resource/clusterconfigmap/desired.go
@@ -25,7 +25,7 @@ func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) ([]*cor
 	var configMaps []*corev1.ConfigMap
 
 	if key.IsDeleted(&cr) {
-		r.logger.Debugf(ctx, "deleting cluster config maps for workload cluster %#q", key.ClusterID(&cr))
+		r.logger.Debugf(ctx, "deleting cluster configmaps for workload cluster %#q", key.ClusterID(&cr))
 		return configMaps, nil
 	}
 
