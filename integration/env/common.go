359ca5d5b356fa592c98892e24efda902bf6a64d
Ross Fairbanks <rossf7@users.noreply.github.com>
2021-06-03 16:22:47 +0200

    Fix helm templating and add smoke test (#33)
    * Fix helm templating
    
    * `Add smoke test
    
    * Revert go.mod change
    
    * Improve log messages

---
+++ integration/env/common.go
@@ -0,0 +1,62 @@
+// +build k8srequired
+
+package env
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	// EnvVarCircleCI is the process environment variable representing the
+	// CIRCLECI env var.
+	EnvVarCircleCI = "CIRCLECI"
+	// EnvVarCircleSHA is the process environment variable representing the
+	// CIRCLE_SHA1 env var.
+	EnvVarCircleSHA = "CIRCLE_SHA1"
+	// EnvVarE2EKubeconfig is the process environment variable representing the
+	// E2E_KUBECONFIG env var.
+	EnvVarE2EKubeconfig = "E2E_KUBECONFIG"
+	// EnvVarKeepResources is the process environment variable representing the
+	// KEEP_RESOURCES env var.
+	EnvVarKeepResources = "KEEP_RESOURCES"
+)
+
+var (
+	circleCI      string
+	circleSHA     string
+	keepResources string
+	kubeconfig    string
+)
+
+func init() {
+	circleCI = os.Getenv(EnvVarCircleCI)
+	keepResources = os.Getenv(EnvVarKeepResources)
+
+	circleSHA = os.Getenv(EnvVarCircleSHA)
+	if circleSHA == "" {
+		panic(fmt.Sprintf("env var '%s' must not be empty", EnvVarCircleSHA))
+	}
+
+	kubeconfig = os.Getenv(EnvVarE2EKubeconfig)
+	if kubeconfig == "" {
+		panic(fmt.Sprintf("env var '%s' must not be empty", EnvVarE2EKubeconfig))
+	}
+}
+
+func CircleCI() bool {
+	return circleCI == strings.ToLower("true")
+}
+
+func CircleSHA() string {
+	return circleSHA
+}
+
+func KeepResources() bool {
+	return keepResources == strings.ToLower("true")
+}
+
+func KubeConfigPath() string {
+	return kubeconfig
+}
