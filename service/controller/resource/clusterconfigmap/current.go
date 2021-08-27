b89a82315205bca6a8dbedde89268a936dcf4d66
Christian Bianchi <whites11@gmail.com>
2021-08-27 13:58:07 +0200

    Add Cluster CIDR in the configmap.

--- service/controller/resource/clusterconfigmap/current.go
+++ service/controller/resource/clusterconfigmap/current.go
@@ -27,7 +27,7 @@ func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) ([]*cor
 			LabelSelector: fmt.Sprintf("%s=%s", label.ManagedBy, project.Name()),
 		}
 
-		list, err := r.k8sClient.CoreV1().ConfigMaps(key.ClusterID(&cr)).List(ctx, lo)
+		list, err := r.k8sClient.K8sClient().CoreV1().ConfigMaps(key.ClusterID(&cr)).List(ctx, lo)
 		if err != nil {
 			return nil, microerror.Mask(err)
 		}
