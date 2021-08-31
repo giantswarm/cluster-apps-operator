15b3e0378624f4e36d0e36c01e98f8cef62c9621
GitHub Action <action@github.com>
2021-08-31 11:58:15 +0000

    Bump version to 0.6.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-apps-operator"
 	source      = "https://github.com/giantswarm/cluster-apps-operator"
-	version     = "0.6.0"
+	version     = "0.6.1-dev"
 )
 
 func Description() string {
