ada01b691e6a0e8a075a8773f7520655646b8097
Ross Fairbanks <rossf7@users.noreply.github.com>
2022-01-13 11:24:38 +0100

    Add required values to integration test

---
+++ integration/templates/operator_values.go
@@ -0,0 +1,9 @@
+package templates
+
+// ClusterAppOperatorValues values required by the cluster-apps-operator chart.
+const ClusterAppOperatorValues = `
+ provider:
+   kind: aws
+ registry:
+   domain: quay.io
+`
