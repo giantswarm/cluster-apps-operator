6b1058d8c4917da9a4c14f73df05a6efa3710c88
Thomas Fussell <thomas@fussell.io>
2022-02-10 13:47:53 -0500

    integration test value

--- integration/templates/operator_values.go
+++ integration/templates/operator_values.go
@@ -2,6 +2,7 @@ package templates
 
 // ClusterAppsOperatorValues values required by the cluster-apps-operator chart.
 const ClusterAppsOperatorValues = `
+ baseDomain: example.com
  provider:
    kind: aws
  registry:
