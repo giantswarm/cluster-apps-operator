fc80abf6f92b7e6aa53381a454f7d4f0c2d1aedf
taylorbot <dev@giantswarm.io>
2023-11-14 15:27:33 +0000

    Bump version to 2.16.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-apps-operator"
 	source      = "https://github.com/giantswarm/cluster-apps-operator"
-	version     = "2.16.0"
+	version     = "2.16.1-dev"
 )
 
 func Description() string {
