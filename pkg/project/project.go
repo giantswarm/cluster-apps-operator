69d2b223e79e2368fdcc645a929734ebd5dbab14
taylorbot <dev@giantswarm.io>
2023-11-15 08:41:04 +0000

    Release v2.17.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-apps-operator"
 	source      = "https://github.com/giantswarm/cluster-apps-operator"
-	version     = "2.16.1-dev"
+	version     = "2.17.0"
 )
 
 func Description() string {
