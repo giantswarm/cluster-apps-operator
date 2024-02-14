c2b530a454b80433317de25ca9387381c8229c4e
Christian Bianchi <whites11@gmail.com>
2024-02-14 09:27:58 +0100

    Do not generate proxy env secret for non private clusters.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Do not generate proxy env secret for non private clusters.
+
 ## [2.20.0] - 2024-02-12
 
 ### Changed
--- service/controller/resource/clustersecret/desired.go
+++ service/controller/resource/clustersecret/desired.go
@@ -23,6 +23,7 @@ import (
 	"github.com/giantswarm/cluster-apps-operator/v2/pkg/project"
 	"github.com/giantswarm/cluster-apps-operator/v2/service/controller/key"
 	infra "github.com/giantswarm/cluster-apps-operator/v2/service/internal/infrastructure"
+	"github.com/giantswarm/cluster-apps-operator/v2/service/internal/privatecluster"
 )
 
 const (
@@ -53,6 +54,10 @@ func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) ([]*cor
 	}
 
 	values := map[string]interface{}{}
+	privateCluster, err := privatecluster.IsPrivateCluster(ctx, r.logger, r.k8sClient.CtrlClient(), cr)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
 
 	{
 		infrastructureRef := cr.Spec.InfrastructureRef
@@ -81,7 +86,7 @@ func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) ([]*cor
 		},
 	}
 
-	if !reflect.ValueOf(r.proxy).IsZero() {
+	if privateCluster && !reflect.ValueOf(r.proxy).IsZero() {
 		r.logger.Debugf(ctx, "proxy secrets for cluster '%s/%s' : %v", cr.GetNamespace(), key.ClusterID(&cr), r.proxy)
 
 		noProxy := noProxy(cr, r.proxy.NoProxy)
