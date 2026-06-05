9db1cde693715ef271e51e921988208136bdab07
taylorbot <dev@giantswarm.io>
2026-06-05 13:45:41 +0000

    chore: bump version to 3.8.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "cluster-apps-operator"
 	source      = "https://github.com/giantswarm/cluster-apps-operator"
-	version     = "3.8.0"
+	version     = "3.8.1-dev"
 )
 
 func Description() string {
